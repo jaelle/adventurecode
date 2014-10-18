581a7fa05be6c6db2e7975bd4f7980b4c6072f71
Jaelle Scheuerman <jaelle@gmail.com>
2014-10-18 16:25:59 -0500

    solve maze puzzles with blockly... still need to figure out timeout

--- app/assets/javascripts/blocklymaze.js
+++ app/assets/javascripts/blocklymaze.js
@@ -1,27 +1,69 @@
 
 Blockly.JavaScript['move_west'] = function(block) {
   // TODO: Assemble JavaScript into code variable.
-  var code = '...';
+  var code = 'moveWest();\n';
   return code;
 };
 
 
 Blockly.JavaScript['move_north'] = function(block) {
   // TODO: Assemble JavaScript into code variable.
-  var code = '...';
+  var code = 'moveNorth();\n';
   return code;
 };
 
 
 Blockly.JavaScript['move_east'] = function(block) {
   // TODO: Assemble JavaScript into code variable.
-  var code = '...';
+  var code = 'moveEast();\n';
   return code;
 };
 
 
 Blockly.JavaScript['move_south'] = function(block) {
   // TODO: Assemble JavaScript into code variable.
-  var code = '...';
+  var code = 'moveSouth();\n';
   return code;
 };
+
+Blockly.JavaScript['loop_forever'] = function(block) {
+  var statements_loop = Blockly.JavaScript.statementToCode(block, 'loop');
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'while {\n' + statements_loop + '\n}';
+  return code;
+};
+
+Blockly.JavaScript['loop_n_times'] = function(block) {
+  var statements_loop = Blockly.JavaScript.statementToCode(block, 'loop');
+  var n = block.getFieldValue('n');
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'for (var i=0;i<' + n + ';i++) {\n';
+  code += statements_loop;
+  code += '\n}';
+
+  return code;
+};
+
+function moveNorth()
+{
+  setTimeout(function() {
+    myMazeLayout.move("north");
+  },500);
+}
+function moveEast()
+{
+  setTimeout(function() {
+    myMazeLayout.move("east");
+  },500)
+
+}
+function moveSouth()
+{
+  myMazeLayout.move("south");
+
+}
+function moveWest()
+{
+  myMazeLayout.move("west");
+
+}
