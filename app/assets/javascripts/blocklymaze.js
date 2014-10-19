0fb0089ac013a2d0fbd0632a6841987cdda55a6a
Jaelle Scheuerman <jaelle@gmail.com>
2014-10-19 13:09:59 -0500

    solve maze puzzles with blockly... still need to figure out timeout

--- app/assets/javascripts/blocklymaze.js
+++ app/assets/javascripts/blocklymaze.js
@@ -26,44 +26,15 @@ Blockly.JavaScript['move_south'] = function(block) {
   return code;
 };
 
-Blockly.JavaScript['loop_forever'] = function(block) {
-  var statements_loop = Blockly.JavaScript.statementToCode(block, 'loop');
-  // TODO: Assemble JavaScript into code variable.
-  var code = 'while {\n' + statements_loop + '\n}';
-  return code;
-};
-
 Blockly.JavaScript['loop_n_times'] = function(block) {
   var statements_loop = Blockly.JavaScript.statementToCode(block, 'loop');
   var n = block.getFieldValue('n');
   // TODO: Assemble JavaScript into code variable.
-  var code = 'for (var i=0;i<' + n + ';i++) {\n';
-  code += statements_loop;
-  code += '\n}';
+  var code = '';
+  
+  for (var i=0;i<n;i++) {
+    code += statements_loop + '\n';
+  }
 
   return code;
 };
-
-function moveNorth()
-{
-  setTimeout(function() {
-    myMazeLayout.move("north");
-  },500);
-}
-function moveEast()
-{
-  setTimeout(function() {
-    myMazeLayout.move("east");
-  },500)
-
-}
-function moveSouth()
-{
-  myMazeLayout.move("south");
-
-}
-function moveWest()
-{
-  myMazeLayout.move("west");
-
-}
